b1c49b61c017f3bdb05412a17c45ea880903dca4
alexey20072401 <alexey20072401@gmail.com>
2026-03-31 17:07:21 +0300

    Функционал для авторизации, с выводом роли на главную страницу

---
+++ Windows/LoginWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Chem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chem.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public DbSet<User> Users { get; set; }
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGuest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var currentuser = App.Context.User
+                .FirstOrDefault(p => p.Login == tbLogin.Text && p.Password == pbPassword.Password);
+            if (currentuser != null) {
+                App.CurrentUser = currentuser;
+                MainWindow main = new MainWindow(currentuser);
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка входа");
+            }
+        }
+    }
+}
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Chem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(Entities.User Role)
+        {
+            InitializeComponent();
+            User currentUser = App.CurrentUser;
+            if (currentUser != null) {
+                tbFio.Text = currentUser.FirstName;
+            }
+            else
+            {
+                tbFio.Text = "Пользователь не определен";
+            }
+            DataContext = this;
+
+        }
+
+        private void ConfigureUIByRole()
+        {
+            //switch (CurrentUserRole)
+            //{
+            //    case "Admin":
+            //        AdminPanel.Visibility = Visibility.Visible;
+            //        ManagerPanel.Visibility = Visibility.Visible;
+            //        break;
+            //    case "Manager":
+            //        AdminPanel.Visibility = Visibility.Collapsed;
+            //        ManagerPanel.Visibility = Visibility.Visible;
+            //        break;
+            //    case "User":
+            //        AdminPanel.Visibility = Visibility.Collapsed;
+            //        ManagerPanel.Visibility = Visibility.Collapsed;
+            //        break;
+            //}
+
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnProducts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnOrders_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
